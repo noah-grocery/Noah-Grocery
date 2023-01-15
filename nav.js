2d0ed2f4bfb6f725c2993133128c8e593f332dc6
noah-grocery <115108254+noah-grocery@users.noreply.github.com>
2023-01-15 09:47:56 -0600

    fix

--- nav.js
+++ nav.js
@@ -1,18 +1,18 @@
-@import 'nav.css';
+
 
 const createNav = () => {
     let nav = document.querySelector('.navbar');
 
     nav.innerHTML = `
         <div class="nav">
-            <img src="Downloads/Noah Grocery Logo(4).png" class="brand-logo" alt="">
+            <img src="Noah Grocery Logo(4).png" class="brand-logo" alt="">
             <div class="nav-items">
                 <div class="search">
                     <input type="text" class="search-box" placeholder="search brand, product">
                     <button class="search-btn">search</button>
                 </div>
-                <a href="login.html"><img src="profile.png" alt=""></a>
-                <a href="cart.html"><img src="cart.png" alt=""></a>
+                <a href="login.html"><img src="profile (2).png" alt=""></a>
+                <a href="cart.html"><img src="cart (2).png" alt=""></a>
             </div>
         </div>
         <ul class="links-container">
